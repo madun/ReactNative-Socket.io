e109b3ecfe8b51a1cc0b0a1c4ed0f8f95838f34f
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-05-19 10:18:16 +0700

    Stuck on creating conversationId at table messages

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Login from './components/Login';
+import Users from './components/Users';
+import Chat from './components/Chat';
+import {createAppContainer} from 'react-navigation';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Login: {
+      screen: Login,
+    },
+    Users: {
+      screen: Users,
+    },
+    Chat: {
+      screen: Chat,
+    },
+  },
+  {
+    initialRouteName: 'Login',
+    navigationOptions: {
+      headerTitle: 'Chat!',
+    },
+  },
+);
+const AppContainer = createAppContainer(RootStack);
